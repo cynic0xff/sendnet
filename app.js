0e0ebca637268e5c4a21dab2139805912ef5a809
Wren <scott.wren@nrtsoutheast.com>
2019-02-11 15:09:53 -0500

    Add basic Sendnet class

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const path = require('path');
 const fs = require('fs');
 const Excel = require('exceljs');
+let Sendnet = require('./sendnet');
 
+const COLUMN_NAME = {
+    'region': 0,
+    'title': 1,
+    'image_url': 2,
+    'grand_prize' : 3,
+    'additional_prizes': 4,
+    'entry_date': 5,
+    'giveaway_url': 6
+};
 
-
-console.log('loading...');
+let sendnet = new Sendnet(COLUMN_NAME);
---
+++ sendnet.js
@@ -0,0 +1,8 @@
+class Sendnet {
+    constructor(COLUMN_NAME) {
+        console.log('Loading Sendnet');
+        console.log(COLUMN_NAME);
+    }
+}
+
+module.exports = Sendnet;
